28fc8e796f7fe18aa7edfc2029a89d19100f8a88
Simon Urbanek <simon.urbanek@r-project.org>
2012-04-03 03:42:58 +0000

    make quadratic method the default in thin()
    git-svn-id: svn://svn.rforge.net/fastshp/trunk@12 ea594f2d-a6d2-4107-8a55-ab247ddb1e69

--- R/thin.R
+++ R/thin.R
@@ -1,1 +1,1 @@
-thin <- function(x, y, tolerance = 1e-4, lock = NULL, method = 1L) .Call("thin", x, y, tolerance, lock, method)
+thin <- function(x, y, tolerance = 1e-4, lock = NULL, method = 2L) .Call("thin", x, y, tolerance, lock, method)
