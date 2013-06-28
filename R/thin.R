0729cadaf20908bb6a9e7dfe6fb1760ecd58d0c4
Simon Urbanek <simon.urbanek@r-project.org>
2013-06-28 18:39:20 +0000

    treat parts properly in thin.shp
    git-svn-id: svn://svn.rforge.net/fastshp/trunk@30 ea594f2d-a6d2-4107-8a55-ab247ddb1e69

--- R/thin.R
+++ R/thin.R
@@ -7,7 +7,8 @@ thin.shp <- function(shp, tolerance = 1e-3, max.width = 5L, all = !is.data.frame
   if (!is.data.frame(shp) || !identical(names(shp), c("id","type","part","x","y")))
     stop("`shp' must be a shape list as returned by read.shp(.., format='table')")
   o <- order(shp$x, shp$y)
-  t <- .Call("atag", shp$x, shp$y, shp$id, o, max.width, PACKAGE="fastshp") 
+  xp <- max(shp$part) + 2L ## we'll generate synthetic IDs that consist of both part and id
+  t <- .Call("atag", shp$x, shp$y, shp$id * xp + shp$part, o, max.width, PACKAGE="fastshp") 
   h <- thin(shp$x, shp$y, tolerance, t$fix, id = shp$id)
   if (all) {
     shp$thin <- h[t$ref]
